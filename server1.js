ad1a462b6a55ed0d5a097b18f8e5286f9b2c0e8f
Shoaib Waqar <88877078+Shin0b0@users.noreply.github.com>
2023-03-22 07:41:25 +0500

    sever

--- server1.js
+++ server1.js
@@ -1,46 +1,79 @@
-const express = require("express");
-const app = express();
-const indexRoutes = require("./routes/index.route");
-const util = require("util");
+// // <<<<<<< HEAD
+// const express = require("express");
+// const app = express();
+// const indexRoutes = require("./routes/index.route");
+// // const util = require("util");
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "shoaibLOGIN",
-});
+// // var mysql = require("mysql");
+// // var connection = mysql.createConnection({
+// //   host: "localhost",
+// //   user: "root",
+// //   password: "",
+// //   database: "shoaibLOGIN",
+// // });
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// // connection.connect(function (err) {
+// //   if (err) {
+// //     console.error("error connecting: " + err.stack);
+// //     return;
+// //   }
 
-  console.log("connected as ");
-});
+// //   console.log("connected as ");
+// // =======
+// // // routes/about.js
+// // const express = require("express");
+// // const app = express();
+// // const router = express.Router();
+// // const i = require("./routes/index");
+// // app.set('view engine', 'ejs');
 
-const query = util.promisify(connection.query).bind(connection);
+// // const mysql = require("mysql");
+// // const util = require("util");
+// // const connection = mysql.createConnection({
+// //   host: "localhost",
+// //   user: "root",
+// //   password: "",
+// //   database: "cafe",
+// // >>>>>>> d437d24 (The ui)
+// // });
 
-async function fetchData() {
-  try {
-    const results = await query("SELECT * FROM shinobi ");
-    console.log("The data is: ", results);
-    return results;
-  } catch (error) {
-    console.error(error);
-  }
-}
+// // const query = util.promisify(connection.query).bind(connection);
 
-app.use("/", indexRoutes);
+// // <<<<<<< HEAD
+// async function fetchData() {
+//   try {
+//     const results = await query("SELECT * FROM shinobi ");
+//     console.log("The data is: ", results);
+//     return results;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-app.listen(3000, async () => {
-  try {
-    const data = await fetchData();
-    console.log("Data fetched successfully!");
-  } catch (error) {
-    console.error("Error fetching data: ", error);
-  } finally {
-    connection.end();
-  }
-});
+// app.use("/", indexRoutes);
+
+// app.listen(3000, async () => {
+//   try {
+//     const data = await fetchData();
+//     console.log("Data fetched successfully!");
+//   } catch (error) {
+//     console.error("Error fetching data: ", error);
+//   } finally {
+//     connection.end();
+//   }
+// });
+// // =======
+// // app.use("/", i);
+
+// // const startServer = async () => {
+// //   try {
+// //     await query("select * from logo");
+// //     console.log("The movie is :", result[0]);
+// //     app.listen(3000);
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // };
+
+// // startServer();
+// // >>>>>>> d437d24 (The ui)
